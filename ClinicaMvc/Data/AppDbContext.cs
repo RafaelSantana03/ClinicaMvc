d61162f00bf62fb86fd635bad6c47a4f4fe49740
Rafael Santana <rafa03santana@gmail.com>
2026-04-05 14:30:06 -0300

    feat: conecta as entidades com banco de dados

---
+++ ClinicaMvc/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ClinicaMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicaMvc.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Consulta> Consultas { get; set; } 
+}
