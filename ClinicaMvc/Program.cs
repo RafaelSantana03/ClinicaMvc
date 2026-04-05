12b94e24195ca26f5df99ed81ae5daba7df042e3
Rafael Santana <rafa03santana@gmail.com>
2026-04-05 14:26:47 -0300

    chore: registra os servicos de banco de dados

--- ClinicaMvc/Program.cs
+++ ClinicaMvc/Program.cs
@@ -1,7 +1,13 @@
+using ClinicaMvc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
