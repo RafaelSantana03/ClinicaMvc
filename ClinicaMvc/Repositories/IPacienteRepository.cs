fd37645089af63d2af8da7cfa79522b8f15e25e5
Rafael Santana <rafa03santana@gmail.com>
2026-04-05 23:41:53 -0300

    feat: cria interface IPacienteRepository

---
+++ ClinicaMvc/Repositories/IPacienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClinicaMvc.Models;
+
+namespace ClinicaMvc.Repositories;
+
+public interface IPacienteRepository
+{
+    List<Paciente> ListarTodos();
+    Paciente? ObterPorId(int id);
+    void Criar(Paciente paciente);
+    void Atualizar(Paciente paciente);
+    void Deletar(Paciente paciente);
+}
