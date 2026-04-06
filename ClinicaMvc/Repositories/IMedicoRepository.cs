258dcbebe6cc554e6fba3692a3cf0cef1b4eae74
Rafael Santana <rafa03santana@gmail.com>
2026-04-05 23:17:06 -0300

    feat: Cria interface IMedicoRepository

---
+++ ClinicaMvc/Repositories/IMedicoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClinicaMvc.Models;
+
+namespace ClinicaMvc.Repositories;
+
+public interface IMedicoRepository
+{
+    // Definir os métodos que serão implementados no MedicoRepository
+    // Listar todos os médicos
+    List<Medico> ListarTodos();
+    Medico? ObterPorId(int id); // ? indica que pode retornar null
+    void Criar(Medico medico); 
+    void Atualizar(Medico medico);
+    void Deletar(Medico medico);
+}
