ac51b31d344fe3f3e1cc41ccf90c977730841d60
Rafael Santana <rafa03santana@gmail.com>
2026-04-05 23:17:34 -0300

    feat: implementa repositorio de Medico

---
+++ ClinicaMvc/Repositories/MedicoRepository.cs
@@ -0,0 +1,40 @@
+﻿using ClinicaMvc.Data;
+using ClinicaMvc.Models;
+
+namespace ClinicaMvc.Repositories;
+
+public class MedicoRepository : IMedicoRepository
+{
+    private readonly AppDbContext _context;
+    public MedicoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public List<Medico> ListarTodos()
+    {
+        return _context.Medicos.ToList();
+    }
+
+    public Medico? ObterPorId(int id)
+    {
+        return _context.Medicos.Find(id);
+    }
+
+    public void Criar(Medico medico)
+    {
+        _context.Medicos.Add(medico);
+        _context.SaveChanges();
+    }
+
+    public void Atualizar(Medico medico)
+    {
+        _context.Medicos.Update(medico);
+        _context.SaveChanges();
+    }
+
+    public void Deletar(Medico medico)
+    {
+        _context.Medicos.Remove(medico);
+        _context.SaveChanges(); 
+    }
+}
