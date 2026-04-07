bd26a9e5772014346b0db72494a13626bd7beb31
Rafael Santana <rafa03santana@gmail.com>
2026-04-06 22:52:28 -0300

    chore: registrando os servicos dos Repositorios

--- ClinicaMvc/Program.cs
+++ ClinicaMvc/Program.cs
@@ -1,4 +1,5 @@
 using ClinicaMvc.Data;
+using ClinicaMvc.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,10 @@ builder.Services.AddControllersWithViews();
 // Banco de dados
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IConsultaRepository, ConsultaRepository>();
+builder.Services.AddScoped<IMedicoRepository, MedicoRepository>();
+builder.Services.AddScoped<IPacienteRepository, PacienteRepository>();
 
 var app = builder.Build();
 
