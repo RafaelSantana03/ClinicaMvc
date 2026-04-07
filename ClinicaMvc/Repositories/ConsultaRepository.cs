d2bb0990eca99f293b98b6b52d66b775ea2af2d5
Rafael Santana <rafa03santana@gmail.com>
2026-04-06 22:37:40 -0300

    feat: implementa repositorio de Consulta

---
+++ ClinicaMvc/Repositories/ConsultaRepository.cs
@@ -0,0 +1,49 @@
+﻿using ClinicaMvc.Models;
+using ClinicaMvc.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicaMvc.Repositories;
+
+public class ConsultaRepository : IConsultaRepository
+{
+    private readonly AppDbContext _context; 
+    public ConsultaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public List<Consulta> ListarTodos()
+    {
+        var consultas = _context.Consultas
+            .Include(c => c.Medico)
+            .Include(c => c.Paciente)
+            .ToList();
+        return consultas;
+    }
+
+    public Consulta? ObterPorId(int id)
+    {
+        return _context.Consultas
+            .Include(c => c.Medico)
+            .Include(c => c.Paciente)
+            .FirstOrDefault(c => c.Id == id);
+    }
+
+    public void Criar(Consulta consulta)
+    {
+        _context.Consultas.Add(consulta);
+        _context.SaveChanges();
+    }
+
+    public void Atualizar(Consulta consulta)
+    {
+        _context.Consultas.Update(consulta);
+        _context.SaveChanges();
+    }
+
+    public void Deletar(Consulta consulta)
+    {
+        _context.Consultas.Remove(consulta);
+        _context.SaveChanges();
+    }
+
+}
