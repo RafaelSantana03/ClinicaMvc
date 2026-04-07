31e279e5e2bbbaa1318a788b48b4207f7f40712f
Rafael Santana <rafa03santana@gmail.com>
2026-04-06 22:36:12 -0300

    feat: cria interface IConsultRepositories

---
+++ ClinicaMvc/Repositories/IConsultaRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClinicaMvc.Models;
+
+namespace ClinicaMvc.Repositories;
+
+public interface IConsultaRepository
+{
+    List<Consulta> ListarTodos();
+    Consulta? ObterPorId(int id);
+    void Criar(Consulta consulta);
+    void Atualizar(Consulta consulta);
+    void Deletar(Consulta consulta);
+}
