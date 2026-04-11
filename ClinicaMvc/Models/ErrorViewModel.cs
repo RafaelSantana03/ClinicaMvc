c6ae7d19ddebb05d524291cbd8d93a0e97c3fd92
Rafael Santana <rafa03santana@gmail.com>
2026-04-10 23:27:45 -0300

    deletando arquivos inutilizados

--- ClinicaMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClinicaMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
