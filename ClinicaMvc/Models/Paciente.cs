578161d42c88f74ac8268b129f2217736f35a452
Rafael Santana <rafa03santana@gmail.com>
2026-04-12 10:22:15 -0300

    feat: aplica dataAnnotations a propriedade 'DataNascimento'

--- ClinicaMvc/Models/Paciente.cs
+++ ClinicaMvc/Models/Paciente.cs
@@ -1,10 +1,13 @@
-﻿namespace ClinicaMvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaMvc.Models;
 
 public class Paciente
 {
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public string CPF { get; set; } = string.Empty;
+    [DataType(DataType.Date)]
     public DateTime DataNascimento { get; set; }
 
     // Navegation Property
