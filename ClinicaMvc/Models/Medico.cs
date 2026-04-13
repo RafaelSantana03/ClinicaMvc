cf7b6fa7a9e06fb1d0da4bfb359ffbc241640d49
Rafael Santana <rafa03santana@gmail.com>
2026-04-13 17:03:57 -0300

    feat: adicionando um enum de especialidades

--- ClinicaMvc/Models/Medico.cs
+++ ClinicaMvc/Models/Medico.cs
@@ -1,12 +1,37 @@
-﻿namespace ClinicaMvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaMvc.Models;
 
 public class Medico
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "O nome do médico é obrigatório.")]
+    [StringLength(100, MinimumLength = 3,
+        ErrorMessage = "O nome deve ter entre 3 e 100 caracteres.")]
     public string Nome { get; set; } = string.Empty; // Tornar obrigatório
+    [Required(ErrorMessage = "O CRM do médico é obrigatório.")]
+    [RegularExpression(@"^\d{1,6}\/[A-Z]{2}$", ErrorMessage = "O CRM deve estar no formato correto (ex: 000000/UF.")]
     public string CRM { get; set; } = string.Empty;
-    public string Especialidade { get; set; } = string.Empty;
+    [Required(ErrorMessage = "A especialidade do médico é obrigatória.")]
+    public Especialidades Especialidade { get; set; }
 
     // Navegation Property
     public ICollection<Consulta> Consultas { get; set; } = new List<Consulta>();
+
+    public enum Especialidades
+    {
+        Cardiologia,
+        Dermatologia,
+        Endocrinologia,
+        Gastroenterologia,
+        Ginecologia,
+        Neurologia,
+        Oftalmologia,
+        Ortopedia,
+        Pediatria,
+        Psiquiatria,
+        Radiologia,
+        Reumatologia,
+        Urologia
+    }
 }
