5d3f4e64337d27343cba2a35f13731da3b721b3e
Rafael Santana <rafa03santana@gmail.com>
2026-04-13 00:49:10 -0300

    feat: adiciona DataAnnotations as propriedades Medico e Paciente

--- ClinicaMvc/Models/Consulta.cs
+++ ClinicaMvc/Models/Consulta.cs
@@ -1,4 +1,7 @@
-﻿namespace ClinicaMvc.Models;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaMvc.Models;
 
 public class Consulta
 {
@@ -8,12 +11,18 @@ public class Consulta
     public int MedicoId { get; set; }
     public int PacienteId { get; set; }
 
+    [Display(Name = "Data e Hora")]
+    [DataType(DataType.DateTime)]
     public DateTime DataHora { get; set; }
+    [Display(Name = "Observações")]
     public string? Observacoes { get; set; } // Tornar opcional
+    [Display(Name = "Status")]
     public StatusConsulta Status { get; set; } = StatusConsulta.Agendada;
 
     // Navegation Properties
+    [ValidateNever]
     public Medico Medico { get; set; } = null!;
+    [ValidateNever]
     public Paciente Paciente { get; set; } = null!;
 
     public enum StatusConsulta
